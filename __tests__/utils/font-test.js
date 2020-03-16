e700982b013e548f9a8f580e7de1ae62b63fd26b
Lucas Bueno <im@lucasbueno.com>
2020-03-16 00:24:06 -0300

    remove consolo.log on  font-test

--- __tests__/utils/font-test.js
+++ __tests__/utils/font-test.js
@@ -3,13 +3,12 @@ import {getFontStyleObject} from '../../src/utils/font';
 describe('utils.fonts', () => {
   describe('getFontStyleObject', () => {
     it('get empty font object', () => {
-      const defaultFontObject = {
+      const result = getFontStyleObject();
+      expect(result).toEqual({
         fontFamily: 'Roboto',
         fontWeight: '400',
         fontStyle: 'normal',
-      };
-      const result = getFontStyleObject();
-      console.log(result);
+      });
     });
   });
 });
