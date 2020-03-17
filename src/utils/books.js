e8126b6e255bf8b36bfbddc399d6fd8a22f6ff71
Lucas Bueno <im@lucasbueno.com>
2020-03-17 09:02:27 -0300

    clear book object

--- src/utils/books.js
+++ src/utils/books.js
@@ -7,4 +7,20 @@ export const filterDuplicateBooks = books =>
     (book, index) => index === books.findIndex(m => bookId(m) === bookId(book)),
   );
 
-const parseBook = book => ({...book});
+const parseBook = b => {
+  const book = {
+    id: b.id,
+    thumbnail: b.volumeInfo.imageLinks.thumbnail,
+    title: b.volumeInfo.title,
+    description: b.volumeInfo.description,
+    authors: b.volumeInfo.authors,
+    publisher: b.volumeInfo.publisher,
+    pageCount: b.volumeInfo.pageCount,
+    language: b.volumeInfo.language,
+    hasEpub: b.accessInfo.epub.isAvailable,
+    hasPdf: b.accessInfo.pdf.isAvailable,
+    priece: b.saleInfo.listPrice,
+  };
+  return {...book};
+};
+//const parseBook = book => ({...book});
